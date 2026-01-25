2f39e3dcb78d8c3fa78347f9c5c08d3b4a66641f
Yago Novaes <yagonovaesneves@gmail.com>
2026-01-24 22:20:47 -0300

    methodology

---
+++ methodology/information-schema.sql
@@ -0,0 +1,25 @@
+select
+    table_name,
+    column_name,
+    data_type,
+    is_nullable
+from `bigquery-public-data.stackoverflow.INFORMATION_SCHEMA.COLUMNS`
+where table_name in (
+    'badges',
+    'comments',
+    'post_history',
+    'post_links',
+    'posts_answers',
+    'posts_moderator_nomination',
+    'posts_orphaned_tag_wiki',
+    'posts_privilege_wiki',
+    'posts_questions',
+    'posts_tag_wiki',
+    'posts_tag_wiki_excerpt',
+    'posts_wiki_placeholder',
+    'stackoverflow_posts',
+    'tags',
+    'users',
+    'votes'
+)
+order by table_name, ordinal_position;
